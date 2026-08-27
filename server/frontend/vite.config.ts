848f3f36db30238efa23b5bbc08f98bd180ca4ca
partialy <partialy@163.com>
2026-08-27 10:26:32 +0800

    功能（server）：统一服务端与前端构建产物

--- server/frontend/vite.config.ts
+++ server/frontend/vite.config.ts
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "../web-user",
+    emptyOutDir: true,
+  },
   server: {
     port: 5174,
     proxy: {
